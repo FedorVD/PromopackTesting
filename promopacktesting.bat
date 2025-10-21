d8e3e48aa895a10daad8693fc2bde5fbb2b4fb71
Федор Дмитриев <fedorvd@mail.ru>
2025-10-21 16:19:04 +0300

    prod commit

---
+++ promopacktesting.bat
@@ -0,0 +1,4 @@
+$env:DB_URL="jdbc:postgresql://localhost:5433/promopacktesting_db?createDatabaseIfNotExist=true"
+$env:DB_USERNAME="postgres"
+$env:DB_PASSWORD="postgres"
+java -jar your-application-name.jar
