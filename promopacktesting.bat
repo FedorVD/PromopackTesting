cf9a7108adb5a27eaa6b2684a53f50d39a0b1155
Федор Дмитриев <fedorvd@mail.ru>
2025-11-18 08:15:27 +0300

    2025_11_18

--- promopacktesting.bat
+++
@@ -1,4 +0,0 @@
-set DB_URL=jdbc:postgresql://localhost:5433/promopacktesting_db?createDatabaseIfNotExist=true
-set DB_USERNAME=postgres
-set DB_PASSWORD=postgres
-java -jar PromopackTesting-0.0.1-SNAPSHOT.jar
